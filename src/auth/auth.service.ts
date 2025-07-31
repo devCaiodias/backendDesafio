5e0423f2dbf9a881f4e1f51df79cdf3c98d53564
devCaiodias <caiodiasmartins12@gmail.com>
2025-07-31 10:30:27 -0300

    refator: atualize os nomes das propriedades de carga útil do JWT para obter consistência

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
     }
 
     async login(user: any) {
-        const payload = { sub: user.id , email: user.email }
+        const payload = { userId: user.id , email: user.email }
         return {
             access_token: this.jwtService.sign(payload),
         };
